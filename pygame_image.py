505bc085062a801214ac444f72f5240dd20b7076
Daichi <c0a2209729@edu.teu.ac.jp>
2023-11-21 17:32:08 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -7,21 +7,35 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
-    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,10,1.0)]
+    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,2.5,1.0),pg.transform.rotozoom(kk_img,5,1.0),pg.transform.rotozoom(kk_img,7.5,1.0),pg.transform.rotozoom(kk_img,10,1.0)]
     tmr = 0
+    i = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        if tmr%2 == 0:
+        if (tmr >= 800) and (tmr <= 3200):
+            screen.blit(bg_img2,[-tmr+1600,0])
+        if tmr <= 1600:
+            screen.blit(bg_img,[-tmr,0])
+        if tmr >= 2400:
+            screen.blit(bg_img,[-tmr+3200,0])
+        if tmr == 3200:
+            tmr = 0
+        if tmr%5 == 4:
             screen.blit(kk_imgs[0],[300,200])
-        elif tmr%2 == 1:
+        if tmr%5 == 3:
             screen.blit(kk_imgs[1],[300,200])
+        if tmr%5 == 2:
+            screen.blit(kk_imgs[2],[300,200])
+        if tmr%5 == 1:
+            screen.blit(kk_imgs[3],[300,200])
+        if tmr%5 == 0:
+            screen.blit(kk_imgs[4],[300,200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1     
         clock.tick(10)
 
 
